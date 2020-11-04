6c270d78324f5e033c16beb151cafc2e82ff0a58
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2020-11-04 03:56:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 class Shoe
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
@@ -6,10 +7,13 @@ class Shoe
     @brand = brand
   end
 
+  def brand
+    @brand
+  end
+
   def cobble
     @condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 
 end
